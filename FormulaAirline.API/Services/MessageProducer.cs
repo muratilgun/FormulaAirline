03253d3081bdf0445327f943f2aac563bf54fd60
Murat Ilgun <muratilgun34@gmail.com>
2023-08-17 20:46:20 +0300

    Added Rabbitmq

---
+++ FormulaAirline.API/Services/MessageProducer.cs
@@ -0,0 +1,9 @@
+namespace FormulaAirline.API.Services;
+
+public class MessageProducer : IMessageProducer
+{
+    public void SendMessage<T>(T message)
+    {
+        throw new NotImplementedException();
+    }
+}
