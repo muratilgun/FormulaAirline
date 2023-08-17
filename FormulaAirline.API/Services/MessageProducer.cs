49f160123944a4588ce9d6a91a79114ec91ddb0c
Murat Ilgun <muratilgun34@gmail.com>
2023-08-17 21:28:38 +0300

    rabbbitmq config.

--- FormulaAirline.API/Services/MessageProducer.cs
+++ FormulaAirline.API/Services/MessageProducer.cs
@@ -1,9 +1,27 @@
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
 namespace FormulaAirline.API.Services;
 
 public class MessageProducer : IMessageProducer
 {
     public void SendMessage<T>(T message)
     {
-        throw new NotImplementedException();
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            UserName = "user",
+            Password = "mypass",
+            VirtualHost = "/"
+        };
+
+        var conn = factory.CreateConnection();
+        using var channel = conn.CreateModel();
+        channel.QueueDeclare(queue: "bookings", durable: true, exclusive: true);
+        var jsonString = JsonSerializer.Serialize(message);
+        var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(jsonString));
+        channel.BasicPublish(exchange:"", routingKey:"bookings", body: body);
+        
     }
 }
